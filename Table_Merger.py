a9e29deb40367534c55997bc3ec7020a51f064d0
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2023-07-25 05:42:20 -0500

    Update Table_Merger.py

--- Table_Merger.py
+++ Table_Merger.py
@@ -1,26 +1,35 @@
 import pandas as pd
 
 def merge_excel_files(file1, file2, sheet1, sheet2, output_file):
-    # Read the Excel file
-    excel_file1 = pd.ExcelFile(file1)
-    excel_file2 = pd.ExcelFile(file2)
+    try:
+        # Read the Excel files
+        excel_file1 = pd.ExcelFile(file1)
+        excel_file2 = pd.ExcelFile(file2)
 
-    # Read the specified sheets from the Excel file
-    df1 = excel_file1.parse(sheet1)
-    df2 = excel_file2.parse(sheet2)
+        # Read the specified sheets from the Excel files
+        df1 = excel_file1.parse(sheet1)
+        df2 = excel_file2.parse(sheet2)
 
-    # Merge the dataframes based on the 'Well Number' column
-    merged_df = pd.merge(df1, df2, on='WI Unique Well No', how='inner')
+        # Merge the dataframes based on the 'Well Number' column
+        merged_df = pd.merge(df1, df2, on='WI Unique Well No', how='inner')
 
-    # Save the merged dataframe to a new Excel file
-    merged_df.to_excel(output_file, index=False)
-    print(f"Merged data saved to {output_file}")
+        # Save the merged dataframe to a new Excel file
+        merged_df.to_excel(output_file, index=False)
+        print(f"Merged data saved to {output_file}")
+    except FileNotFoundError:
+        print(f"One or both of the files {file1} and {file2} do not exist.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-# Example usage
-file1 = 'Updated_Radium_Data.xlsx'
-file2 = 'Sand and Gravel filtered DNR.xlsx'
-sheet1 = 'Radium'
-sheet2 = 'Sheet1'
-output_file = 'All Wells Ra DNR Merged version 2.xlsx'
+def main():
+    # Example usage
+    file1 = 'Updated_Radium_Data.xlsx'
+    file2 = 'Sand and Gravel filtered DNR.xlsx'
+    sheet1 = 'Radium'
+    sheet2 = 'Sheet1'
+    output_file = 'All Wells Ra DNR Merged version 2.xlsx'
 
-merge_excel_files(file1,file2, sheet1, sheet2, output_file)
+    merge_excel_files(file1, file2, sheet1, sheet2, output_file)
+
+if __name__ == "__main__":
+    main()

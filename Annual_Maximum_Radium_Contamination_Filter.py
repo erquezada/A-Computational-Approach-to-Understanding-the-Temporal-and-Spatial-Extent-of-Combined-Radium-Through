8e5c5178a08fa0a4cb156a4db6ecea11879f1ce2
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2023-07-25 12:09:17 -0500

    Update Annual_Maximum_Radium_Contamination_Filter.py

--- Annual_Maximum_Radium_Contamination_Filter.py
+++ Annual_Maximum_Radium_Contamination_Filter.py
@@ -20,15 +20,16 @@ def read_excel_file(file_path, sheet_name=None):
 def wells_sampled(df):
     df['Sample Date'] = pd.to_datetime(df['Sample Date'])
     df['Year'] = df['Sample Date'].dt.year
-    df.sort_values(['WI_UNIQUE_', 'Year', 'Measured A'], ascending=[True, True, False], inplace=True)
-    df.drop_duplicates(['WI_UNIQUE_', 'Year'], keep='first', inplace=True)
-    df['Count'] = df.groupby(['WI_UNIQUE_', 'Year'])['Measured A'].transform('count')
+    df.sort_values(['WI_UNIQUE_WELL_NO', 'Year', 'Measured Amount'], ascending=[True, True, False], inplace=True)
+    df.drop_duplicates(['WI_UNIQUE_WELL_NO', 'Year'], keep='first', inplace=True)
+    df['Count'] = df.groupby(['WI_UNIQUE_WELL_NO', 'Year'])['Measured Amount'].transform('count')
     return df
 
 def main():
-    file_path = "/Users/erquezada/Desktop/Max_Contamination/CO_U and CO_C Wells with Ra data DNR merged -S&G in MCOAS.xlsx"
-    # Get the sheet name from the user
-    sheet_name = input("Enter the sheet name you want to work with: ")
+    file_path = "filtered_sand_and_gravel_wells_dnr.xlsx"
+    
+    sheet_name = "Sheet1"
+    
 
     df = read_excel_file(file_path, sheet_name=sheet_name)
 
@@ -41,7 +42,7 @@ def main():
         script_dir = os.path.dirname(os.path.realpath(__file__))
 
         # Write data to an Excel file
-        output_file_name = os.path.join(script_dir, f"updated_wells_data_{sheet_name}.xlsx")
+        output_file_name = os.path.join(script_dir, f"filtered_annual_maximum_radium.xlsx")
         wells_data.to_excel(output_file_name, index=False)
         print(f"Updated wells data has been written to '{output_file_name}'.")
     else:

78aa62f78e78dc2a62d6323094fab14faa30b3b3
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2023-07-25 05:44:38 -0500

    Update Well_Information_Populator.py

--- Well_Information_Populator.py
+++ Well_Information_Populator.py
@@ -1,49 +1,64 @@
 import pandas as pd
 
-# Load the data into a DataFrame
-# insert proper file name below. Must be in the format as below
-file_path = "/Users/erquezada/Desktop/All_Wisconsin_Wells_DNR.xlsx"
-# Get the sheet name from the user
-sheet_name = input("Enter the sheet name you want to work with: ")
+def handle_missing_data(data):
+    # Iterate over each row in the DataFrame
+    for index, row in data.iterrows():
+        current_wi_unique = row['WI_UNIQUE_WELL_NO']
+        first_bedrock_ft = row['FIRST_BEDROCK_FT']
 
-data = pd.read_excel('All_Wisconsin_Wells_DNR.xlsx')
+        # Check if FIRST_BEDROCK_FT is empty
+        if pd.isnull(first_bedrock_ft):
+            previous_well_no = row['PREVIOUS_WELL_NO']
+            replacement_well_no = row['REPLACEMENT_WELL_NO']
 
-# Sort the data by WI Unique number
-data = data.sort_values('WI_UNIQUE_WELL_NO')
+            # Check if previous well number is available
+            if pd.notnull(previous_well_no):
+                # Find the previous well's FIRST_BEDROCK_FT
+                previous_row = data[data['WI_UNIQUE_WELL_NO'] == previous_well_no]
 
-# Iterate over each row in the DataFrame
-for index, row in data.iterrows():
-    current_wi_unique = row['WI_UNIQUE_WELL_NO']
-    first_bedrock_ft = row['FIRST_BEDROCK_FT']
+                if len(previous_row) > 0:
+                    previous_bedrock_ft = previous_row['FIRST_BEDROCK_FT'].values[0]
 
-    # Check if FIRST_BEDROCK_FT is empty
-    if pd.isnull(first_bedrock_ft):
-        previous_well_no = row['PREVIOUS_WELL_NO']
-        replacement_well_no = row['REPLACEMENT_WELL_NO']
+                    # Check if previous well's FIRST_BEDROCK_FT is available
+                    if pd.notnull(previous_bedrock_ft):
+                        # Update the current row with the previous well's FIRST_BEDROCK_FT
+                        data.at[index, 'FIRST_BEDROCK_FT'] = previous_bedrock_ft
+            elif pd.notnull(replacement_well_no):
+                # Find the replacement well's FIRST_BEDROCK_FT
+                replacement_row = data[data['WI_UNIQUE_WELL_NO'] == replacement_well_no]
 
-        # Check if previous well number is available
-        if pd.notnull(previous_well_no):
-            # Find the previous well's FIRST_BEDROCK_FT
-            previous_row = data[data['WI_UNIQUE_WELL_NO'] == previous_well_no]
+                if len(replacement_row) > 0:
+                    replacement_bedrock_ft = replacement_row['FIRST_BEDROCK_FT'].values[0]
 
-            if len(previous_row) > 0:
-                previous_bedrock_ft = previous_row['FIRST_BEDROCK_FT'].values[0]
+                    # Check if replacement well's FIRST_BEDROCK_FT is available
+                    if pd.notnull(replacement_bedrock_ft):
+                        # Update the current row with the replacement well's FIRST_BEDROCK_FT
+                        data.at[index, 'FIRST_BEDROCK_FT'] = replacement_bedrock_ft
 
-                # Check if previous well's FIRST_BEDROCK_FT is available
-                if pd.notnull(previous_bedrock_ft):
-                    # Update the current row with the previous well's FIRST_BEDROCK_FT
-                    data.at[index, 'FIRST_BEDROCK_FT'] = previous_bedrock_ft
-        elif pd.notnull(replacement_well_no):
-            # Find the replacement well's FIRST_BEDROCK_FT
-            replacement_row = data[data['WI_UNIQUE_WELL_NO'] == replacement_well_no]
+    return data
 
-            if len(replacement_row) > 0:
-                replacement_bedrock_ft = replacement_row['FIRST_BEDROCK_FT'].values[0]
+def main():
+    try:
+        # Load the data into a DataFrame
+        # Insert proper file name below. Must be in the format as below
+        file_path = "/Users/erquezada/Desktop/All_Wisconsin_Wells_DNR.xlsx"
+        # Get the sheet name from the user
+        sheet_name = input("Enter the sheet name you want to work with: ")
 
-                # Check if replacement well's FIRST_BEDROCK_FT is available
-                if pd.notnull(replacement_bedrock_ft):
-                    # Update the current row with the replacement well's FIRST_BEDROCK_FT
-                    data.at[index, 'FIRST_BEDROCK_FT'] = replacement_bedrock_ft
+        data = pd.read_excel(file_path, sheet_name=sheet_name)
 
-# Save the updated data to a new file
-data.to_excel('updated_data.xlsx', index=False)
+        # Sort the data by WI Unique number
+        data = data.sort_values('WI_UNIQUE_WELL_NO')
+
+        data = handle_missing_data(data)
+
+        # Save the updated data to a new file
+        data.to_excel('updated_data.xlsx', index=False)
+
+    except FileNotFoundError:
+        print(f"The file {file_path} does not exist.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()

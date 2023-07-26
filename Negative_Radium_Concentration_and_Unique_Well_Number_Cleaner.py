147a76a1337d92851ac6d817caaf4f5d7e15f8ad
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2023-07-26 11:29:09 -0500

    Update Negative_Radium_Concentration_and_Unique_Well_Number_Cleaner.py

--- Negative_Radium_Concentration_and_Unique_Well_Number_Cleaner.py
+++ Negative_Radium_Concentration_and_Unique_Well_Number_Cleaner.py
@@ -1,34 +1,25 @@
 import pandas as pd
 
 class ExcelDataReader:
-    def read_excel_file(self, file_path, sheet_name, column_name):
-        df = pd.read_excel(file_path, sheet_name=sheet_name)
-        specific_column = df[column_name]
-        df.index = df.index + 1
-        df.reset_index(drop=True, inplace=True)
+    def __init__(self, file_path, sheet_name):
+        self.df = pd.read_excel(file_path, sheet_name=sheet_name)
+
+    def read_column(self, column_name):
+        specific_column = self.df[column_name]
         return specific_column
 
-def process_excel_file(file_path, sheet_name, column_name, well_number_column):
-    df = pd.read_excel(file_path, sheet_name=sheet_name)
+    def drop_negative_values(self, column_name, well_number_column):
+        # Delete rows with negative values in the specified column
+        self.df = self.df[self.df[column_name] >= 0]
+        # Delete cells with an empty well number
+        self.df = self.df.dropna(subset=[well_number_column])
+        # Reset the index
+        self.df.reset_index(drop=True, inplace=True)
+        return self.df
 
-    # Delete rows with negative values in the specified column
-    df = df[df[column_name] >= 0]
-
-    # Delete cells with an empty well number
-    df = df.dropna(subset=[well_number_column])
-
-    # Reset the index
-    df.reset_index(drop=True, inplace=True)
-
-    return df
-
-def process_unique_well_numbers(df, column_name):
-    unique_well_numbers = df[column_name].unique()
-
-    # Perform further processing or calculations on the unique well numbers
-    # ...
-
-    return unique_well_numbers
+    def process_unique_well_numbers(self, column_name):
+        unique_well_numbers = self.df[column_name].unique()
+        return unique_well_numbers
 
 # Specify the file path, sheet name, and column name
 file_path = 'Ra Contamination in Public Water Supplies.xlsx'
@@ -37,16 +28,16 @@ column_name = 'Measured Amount'
 well_number_column = 'WI_UNIQUE_WELL_NO'
 
 # Create an instance of ExcelDataReader
-data_reader = ExcelDataReader()
+data_reader = ExcelDataReader(file_path, sheet_name)
 
-# Call the read_excel_file method and store the returned column in a variable
-column_data = data_reader.read_excel_file(file_path, sheet_name, column_name)
+# Call the read_column method and store the returned column in a variable
+column_data = data_reader.read_column(column_name)
 
 # Print the column data
 print(column_data)
 
 # Process the Excel file and obtain the updated DataFrame
-processed_data = process_excel_file(file_path, sheet_name, column_name, well_number_column)
+processed_data = data_reader.drop_negative_values(column_name, well_number_column)
 
 # Save the updated DataFrame to a new Excel file
 output_file_path = 'filtered_radium_data_dnr.xlsx'
@@ -55,6 +46,6 @@ processed_data.to_excel(output_file_path, index=False)
 print("\nNew Excel file has been generated:", output_file_path)
 
 # Process the unique well numbers
-unique_wells = process_unique_well_numbers(processed_data, well_number_column)
+unique_wells = data_reader.process_unique_well_numbers(well_number_column)
 print("\nUnique Well Numbers:")
 print(unique_wells)
